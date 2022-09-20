48b001c5a96ca2b116939fd5b8709d2cf3a3037a
unknown <AkshatKhanna1>
2022-09-20 23:07:15 +0530

    bit-manipulation

--- Bit-Manipulation/Decode-XORed-Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def decode(self, encoded: List[int], first: int) -> List[int]:
-        k=first
-        lst=[first]
-        for i in encoded:
-            k=k^i
-            lst.append(k)
-        return lst
-        

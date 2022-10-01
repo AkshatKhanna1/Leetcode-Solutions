d28d40bd18807432ab78a2a9331aef4b50fe062a
AkshatKhanna1 <113794895+AkshatKhanna1@users.noreply.github.com>
2022-10-01 19:32:54 +0530

    sliding-window

---
+++ Sliding-Window/maximum-average-subarray-i.py
@@ -0,0 +1,14 @@
+#O(N) Time Complexity
+#O(1) Space Complexity
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        sum=0
+        n=len(nums)
+        for i in range(k):
+            sum+=nums[i]
+        maxSum=sum
+        for i in range(k,n):
+            sum=sum+nums[i]-nums[i-k]
+            if sum>maxSum:
+                maxSum=sum
+        return maxSum/k

dfadfed7ee2684512a61e3f54c2559efe0d9571f
AkshatKhanna1 <113794895+AkshatKhanna1@users.noreply.github.com>
2022-10-05 00:26:08 +0530

    Prefix-Sum

---
+++ PrefixSum/find-pivot-index.py
@@ -0,0 +1,20 @@
+# O(N) Time Complexity
+# O(1) Space Complexity
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        val=nums[0]
+        n=len(nums)
+
+        for i in range(1,n):
+            val+=nums[i]
+            nums[i]=val
+
+        if nums[n-1]-nums[0]==0:
+            return 0
+
+        for i in range(1,n):
+            if nums[i-1]==nums[n-1]-nums[i]:
+                return i
+
+        return -1

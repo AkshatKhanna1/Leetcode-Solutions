3dd3394be022556056c3f46b27fcb5f57b1da414
AkshatKhanna1 <113794895+AkshatKhanna1@users.noreply.github.com>
2022-10-05 14:04:11 +0530

    Prefix-Sum

---
+++ PrefixSum/subarray-sum-equals-k.py
@@ -0,0 +1,20 @@
+# O(N) Time Complexity
+# O(N) Space Complexity
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSum=0
+        dic={}
+        count=0
+        
+        for i in range(len(nums)):
+            prefixSum+=nums[i]
+            if prefixSum not in dic:
+                dic[prefixSum]=0
+            if prefixSum==k:
+                count+=1
+            if prefixSum-k in dic:
+                count+=dic[prefixSum-k]
+            dic[prefixSum]+=1
+
+        return count

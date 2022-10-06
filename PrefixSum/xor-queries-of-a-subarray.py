2d259d7c5d02cf8f462ea145dc0e21494148dda5
AkshatKhanna1 <113794895+AkshatKhanna1@users.noreply.github.com>
2022-10-06 22:20:13 +0530

    Prefix-Sum

---
+++ PrefixSum/xor-queries-of-a-subarray.py
@@ -0,0 +1,16 @@
+# O(N) Time Complexity
+# O(N) Space Complexity
+
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        xorSum=0
+        answer=[]
+        for i in range(len(arr)):
+            xorSum^=arr[i]
+            arr[i]=xorSum
+        for l,r in queries:
+            if l==0:
+                answer.append(arr[r])
+            else:
+                answer.append(arr[r]^arr[l-1])
+        return answer
